7e50bcb889046ee5f55e83363b7b7c54bcbb44bf
ladroid <puatachok@gmail.com>
2020-02-11 19:06:15 +0200

    Update

---
+++ Fireball.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+const speed = 100
+var velocity = Vector2()
+var direction = 1
+
+func set_fireball_direction(dir):
+	direction = dir
+	if dir == -1:
+		$AnimatedSprite.flip_h = true
+	
+
+func _physics_process(delta):
+	velocity.x = speed * delta * direction
+	translate(velocity)
+	$AnimatedSprite.play("shoot")
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_Fireball_body_entered(body):
+	queue_free()
--- Player.gd
+++ Player.gd
@@ -5,21 +5,42 @@ const gravity = 10
 const jump_power = -250
 const FLOOR = Vector2(0, -1)
 
+const fireball = preload("res://Fireball.tscn")
+
 var velocity = Vector2()
 var on_ground = false
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
+		if sign($Position2D.position.x) == -1:
+			$Position2D.position.x *= -1
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
+		if sign($Position2D.position.x) == 1:
+			$Position2D.position.x *= -1
 	else:
-		velocity.x = 0	
+		velocity.x = 0
+		if on_ground == true:
+			$AnimatedSprite.play("idle")	
 		
 	if Input.is_action_pressed("ui_up"):
 		if on_ground == true:
 			velocity.y = jump_power
 			on_ground = false
+	
+	if Input.is_action_just_pressed("ui_focus_next"):
+		var fire_ball = fireball.instance()
+		if sign($Position2D.position.x) == 1:
+			fire_ball.set_fireball_direction(1)
+		else:
+			fire_ball.set_fireball_direction(-1)
+		get_parent().add_child(fire_ball)
+		fire_ball.position = $Position2D.global_position
 	
 	velocity.y += gravity
 	
@@ -27,5 +48,9 @@ func _physics_process(delta):
 		on_ground = true
 	else:
 		on_ground = false
+		if velocity.y < 0:
+			$AnimatedSprite.play("jump")
+		else:
+			$AnimatedSprite.play("fall")
 	
 	velocity = move_and_slide(velocity, FLOOR)
