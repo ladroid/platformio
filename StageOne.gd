b9c7f0cb41076a605ecef941d206566fd43a7cf9
ladroid <puatachok@gmail.com>
2020-02-14 18:43:45 +0200

    Update: random map

--- StageOne.gd
+++ StageOne.gd
@@ -6,6 +6,10 @@ var spawn_left = Vector2(80, 147.585)
 var spawn_right = Vector2(188, 147.585)
 
 func spawn_enemy():
+	#TODO: when we have infinty platform
+	#make for-loop for enemy 
+	#for i in range(0, 16):
+	#	new_enemy = enemy.instance()
 	var new_enemy = enemy.instance()
 	var temp = null
 	if rand_range(0, 2) > 0:
@@ -17,4 +21,13 @@ func spawn_enemy():
 	
 func _ready():
 	spawn_enemy()
+
+func _process(delta):
+	var mousePos = get_viewport().get_mouse_position()
+	var loc = $TileMap.world_to_map(mousePos)
+	var cell = $TileMap.get_cell(loc.x, loc.y)
+	if(cell != -1):
+		print($TileMap.tile_set.tile_get_name(cell))
+	else:
+		$TileMap.set_cell(loc.x, loc.y,1)
 
