59eed5bdd36c0993cd5a35cfc03d231c9f2a9f34
ladroid <puatachok@gmail.com>
2020-02-18 11:05:45 +0200

    Update: sound, pause, play buttons improvement

--- PauseButtonControl.gd
+++ PauseButtonControl.gd
@@ -4,8 +4,10 @@ var pause = false
 	
 func _process(delta):
 	if pause == true:
+		$Button/AnimatedSprite.play("stop")
 		get_tree().paused = true
 	else:
+		$Button/AnimatedSprite.play("play")
 		get_tree().paused = false
 
 func _on_Button_pressed():
--- SoundButton.gd
+++ SoundButton.gd
@@ -5,8 +5,10 @@ var on = true
 
 func _on_Button_pressed():
 	if on == true:
+		$AnimatedSprite.play("soundon")
 		sound.play()
 		on = false
 	else:
+		$AnimatedSprite.play("soundoff")
 		sound.stop()
 		on = true
