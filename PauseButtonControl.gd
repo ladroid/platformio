cc616974ea69d55757abc70581972ea11968eaea
ladroid <puatachok@gmail.com>
2020-02-18 13:07:34 +0200

    Update: add popupdialog with label and play button, improve pause and menu

--- PauseButtonControl.gd
+++ PauseButtonControl.gd
@@ -1,14 +1,29 @@
 extends Control
 
 var pause = false
-	
+
+func _ready():
+	pause = false
+
 func _process(delta):
 	if pause == true:
 		$Button/AnimatedSprite.play("stop")
 		get_tree().paused = true
+		$PopupDialog.show()
 	else:
-		$Button/AnimatedSprite.play("play")
+		$PopupDialog/unpause/AnimatedSprite.play("play")
 		get_tree().paused = false
+		$PopupDialog.hide()
 
 func _on_Button_pressed():
 	pause = true
+
+
+func _on_unpause_pressed():
+	pause = false
+	print("Pressed!")
+	queue_free()
+
+
+func _on_quit_pressed():
+	get_tree().quit(1)
