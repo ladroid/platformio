e00da8002467a6e8115a0a41328c52fc975d6389
ladroid <puatachok@gmail.com>
2020-02-27 22:53:49 +0200

    Update: stage lavel text

--- ChangeStage.gd
+++ ChangeStage.gd
@@ -6,9 +6,13 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	get_parent().get_node("StagesText/Sprite").hide()
 	$AnimatedSprite.play("flame")
 
 
 func _on_ChangeStage_body_entered(body):
 	if "Player" in body.name:
+		get_parent().get_node("StagesText/Sprite").visible = true
+		get_parent().get_node("StagesText/Control/CenterContainer/Label").text ="A"
 		get_tree().change_scene(target_stage)
+		get_parent().get_node("StagesText/Sprite").hide()
