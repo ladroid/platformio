b1ae2b680d1dd38f223a76124b3b60d5bf50474c
ladroid <puatachok@gmail.com>
2020-03-04 23:28:34 +0200

    Update: improve game, bug fixes, enemy randomize

--- StageOne.gd
+++ StageOne.gd
@@ -3,21 +3,23 @@ extends Node
 export (PackedScene) var enemy
 
 var spawn_left = Vector2(80, 147.585)
-var spawn_right = Vector2(188, 147.585)
+var spawn_right = Vector2(190, 147.585)
 
 func spawn_enemy():
 	#TODO: when we have infinty platform
 	#make for-loop for enemy 
 	#for i in range(0, 16):
 	#	new_enemy = enemy.instance()
-	var new_enemy = enemy.instance()
-	var temp = null
-	if rand_range(0, 2) > 0:
-		temp = spawn_right
-	else:
-		temp = spawn_left
-	new_enemy.set_position(temp)
-	add_child(new_enemy)
+	randomize()
+	for i in rand_range(1, 6):
+		var new_enemy = enemy.instance()
+		var temp = null
+		if rand_range(0, 2) > 0:
+			temp = spawn_right
+		else:
+			temp = spawn_left
+		new_enemy.set_position(temp)
+		add_child(new_enemy)
 	
 func _ready():
 	spawn_enemy()
